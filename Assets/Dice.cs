551c1ac526ccf7357176578c61e9fe7869ac9dcf
lukewaldron20 <44021663+lukewaldron20@users.noreply.github.com>
2020-06-22 14:53:40 +0100

    Added in HighScore script
    added in a highscore script to the space invaders game, that im planning on splitting up into 2 different games

---
+++ Assets/Dice.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Dice : MonoBehaviour{
+
+    public Text score;
+    public Text highScore;
+
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+        
+    }
+
+    public void RollDice()
+    {
+        int number = Random.Range(1, 10000000);
+        score.text = number.ToString();
+
+        if (number > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", number);
+            highScore.text = number.ToString();
+        }
+        
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+        highScore.text = "0";
+    }
+
+}

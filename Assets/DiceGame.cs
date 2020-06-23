1906f0bc87779db65d059ecdff1c7695e2257db6
lukewaldron20 <44021663+lukewaldron20@users.noreply.github.com>
2020-06-23 18:56:19 +0100

    added in new button
    I added in a new game button where i added the Highscore game to the space invaders game, that way you can play both on your device, from the start menu.

---
+++ Assets/DiceGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DiceGame : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
